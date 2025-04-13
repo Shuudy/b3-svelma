64abfc37011dff81f054f7fb773f22a37154f92e
mnoella <mailholnoella@gmail.com>
2025-04-13 22:53:42 +0200

    feat: Gestion des erreurs avec une page d'erreur personnalisée (sans style, à revoir)

---
+++ src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { page } from "$app/stores";
+</script>
+
+<div>
+    <h1>{$page.status}</h1>
+    <p>{$page.error?.message || 'Une erreur est survenue'}</p>
+
+    {#if $page.status === 404}
+        <di>
+            <p>La page que vous recherchez est introuvable.</p>
+        </di>
+    {:else if $page.status === 500}
+        <div>
+            <p>Erreur serveur: Veuillez réessayer plus tard.</p>
+        </div>
+    {/if}
+
+    <a href="/">Retour à l'accueil</a>
+</div>
