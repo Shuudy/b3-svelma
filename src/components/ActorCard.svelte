0ccfe204e8cca517d63cf0548741e6659b0e195d
Shuudy <ancieux.jerome059@gmail.com>
2025-04-24 15:37:26 +0200

    feat: add default profile image for actors without a profile picture

--- src/components/ActorCard.svelte
+++ src/components/ActorCard.svelte
@@ -1,10 +1,12 @@
 <script>
     let { id, profile_path, name, role } = $props();
+
+    const defaultProfile = '/default-profile.svg';
 </script>
 
 <a href={`/personnes/${id}`} class="actor-card">
     <div class="actor-card__image">
-        <img src={`https://image.tmdb.org/t/p/original/${profile_path}`} alt={name} />
+        <img src={profile_path ? `https://image.tmdb.org/t/p/original/${profile_path}` : defaultProfile} alt={name} />
     </div>
     <div class="actor-card__infos">
         <div class="actor-card__name">{name}</div>
