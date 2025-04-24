2bbd479639ec9fc591e51bc477ecae56a92a88ee
Shuudy <ancieux.jerome059@gmail.com>
2025-04-24 11:06:50 +0200

    fix: correct SVG attribute casing in SearchBar component

--- src/components/SearchBar.svelte
+++ src/components/SearchBar.svelte
@@ -3,8 +3,8 @@
         
     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
 
-        <path d="M15.1665 15.1113L18.3333 18.3335" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
-        <path d="M16.6667 9.1665C16.6667 5.02437 13.3089 1.6665 9.16675 1.6665C5.02461 1.6665 1.66675 5.02437 1.66675 9.1665C1.66675 13.3087 5.02461 16.6665 9.16675 16.6665C13.3089 16.6665 16.6667 13.3087 16.6667 9.1665Z" stroke="white" strokeWidth="2" strokeLinejoin="round"/>
+        <path d="M15.1665 15.1113L18.3333 18.3335" stroke="white" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M16.6667 9.1665C16.6667 5.02437 13.3089 1.6665 9.16675 1.6665C5.02461 1.6665 1.66675 5.02437 1.66675 9.1665C1.66675 13.3087 5.02461 16.6665 9.16675 16.6665C13.3089 16.6665 16.6667 13.3087 16.6667 9.1665Z" stroke="white" stroke-width="2" stroke-linejoin="round"/>
     </svg>
 
 </div>
