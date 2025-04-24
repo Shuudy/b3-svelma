d2d87ab4906ac08c8abfd89e6075e4434f4a28a1
Shuudy <ancieux.jerome059@gmail.com>
2025-04-24 10:53:15 +0200

    feat: add SearchBar component for searching movies

---
+++ src/components/SearchBar.svelte
@@ -0,0 +1,28 @@
+<div class="searchbar">
+    <input type="text" placeholder="Rechercher un film ..." name="" id="" />
+        
+    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+
+        <path d="M15.1665 15.1113L18.3333 18.3335" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M16.6667 9.1665C16.6667 5.02437 13.3089 1.6665 9.16675 1.6665C5.02461 1.6665 1.66675 5.02437 1.66675 9.1665C1.66675 13.3087 5.02461 16.6665 9.16675 16.6665C13.3089 16.6665 16.6667 13.3087 16.6667 9.1665Z" stroke="white" strokeWidth="2" strokeLinejoin="round"/>
+    </svg>
+
+</div>
+
+<style>
+    input {
+        padding-right: 30px;
+    }
+
+    .searchbar {
+        position: relative;
+    }
+
+    svg {
+        position: absolute; 
+        top: 50%;
+        right: 10px;
+        transform: translateY(-50%);
+    }
+
+</style>
