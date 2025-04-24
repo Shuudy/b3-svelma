4378ee0010c0f93b68fbedc6ff4e15f42db2c017
Shuudy <ancieux.jerome059@gmail.com>
2025-04-24 11:09:13 +0200

    feat: implement home page with all components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import SearchBar from "../components/SearchBar.svelte";
+    import Filter from "../components/Filter.svelte";
+    import MovieCard from "../components/MovieCard.svelte";
+    import Pagination from "../components/Pagination.svelte";
+</script>
+
+<div class="container">
+	<div class="header">
+		<div class="header__logo">
+			<img src="/svelma.svg" alt="Svelma Logo" />
+		</div>
+		<div class="header__actions">
+			<SearchBar />
+			<Filter />
+		</div>
+	</div>
+	<div class="movie-list">
+		{#each Array(10) as _, index}
+			<MovieCard key={index} id="2" title="Movie title" year="2024" vote_average="5,4" poster_path="/ceG9VzoRAVGwivFU403Wc3AHRys.jpg"  />
+		{/each}
+	</div>
+	<Pagination />
+</div>
