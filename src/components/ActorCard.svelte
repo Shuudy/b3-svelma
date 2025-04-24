5d89bbdd4c41c4012b30911a584dd9cac4a28998
Shuudy <ancieux.jerome059@gmail.com>
2025-04-24 10:32:42 +0200

    feat: add ActorCard component for displaying actor details

---
+++ src/components/ActorCard.svelte
@@ -0,0 +1,13 @@
+<script>
+    let { id, profile_path, name, role } = $props();
+</script>
+
+<a href="/personnes/{id}" class="actor-card">
+    <div class="actor-card__image">
+        <img src="https://image.tmdb.org/t/p/original/{profile_path}" alt="" />
+    </div>
+    <div class="actor-card__infos">
+        <div class="actor-card__name">{name}</div>
+        <div class="actor-card__role">{role}</div>
+    </div>
+</a>
