b254cf8a9c9dd0fb2f6438ed2a10f1979ce15cd7
Shuudy <ancieux.jerome059@gmail.com>
2025-04-24 13:51:59 +0200

    feat: enhance error page layout

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,20 +1,64 @@
 <script>
-    import { page } from "$app/stores";
+	import { page } from '$app/stores';
 </script>
 
-<div>
-    <h1>{$page.status}</h1>
-    <p>{$page.error?.message || 'Une erreur est survenue'}</p>
-
-    {#if $page.status === 404}
-        <di>
-            <p>La page que vous recherchez est introuvable.</p>
-        </di>
-    {:else if $page.status === 500}
-        <div>
-            <p>Erreur serveur: Veuillez réessayer plus tard.</p>
-        </div>
-    {/if}
-
-    <a href="/">Retour à l'accueil</a>
+<div class="error">
+	<div class="error__content">
+		<div class="error__status">{$page.status}</div>
+		<div class="error__message">{$page.error?.message || 'Une erreur est survenue'}</div>
+		<div class="error__description">
+			{#if $page.status === 404}
+				La page que vous recherchez est introuvable.
+			{:else if $page.status === 500}
+				Erreur serveur: Veuillez réessayer plus tard.
+			{/if}
+		</div>
+		<a href="/" class="error__button">Revenir à l'accueil</a>
+	</div>
 </div>
+
+<style>
+	.error {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100dvh;
+	}
+
+	.error__content {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+
+		text-align: center;
+		max-width: 600px;
+		margin: 0 auto;
+	}
+
+	.error__status {
+		font-size: 8rem;
+		font-weight: 700;
+	}
+
+	.error__message {
+		font-size: 1.5rem;
+		font-weight: 700;
+		margin-top: 1rem;
+	}
+
+	.error__description {
+		color: rgb(156, 163, 175);
+		margin-top: 1rem;
+	}
+
+	.error__button {
+		display: flex;
+		background-color: #fff;
+		color: #000;
+		border-radius: 6px;
+		padding: 0.5rem 1rem;
+
+		margin-top: 1rem;
+	}
+</style>
