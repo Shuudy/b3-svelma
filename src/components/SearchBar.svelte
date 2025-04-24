dc94c1c8f5232ce29107a49cc9b146ff3801c232
mnoella <mailholnoella@gmail.com>
2025-04-24 11:33:06 +0200

    feat : search

--- src/components/SearchBar.svelte
+++ src/components/SearchBar.svelte
@@ -1,5 +1,19 @@
+<script>
+	import { searchMovies } from "$lib/api/tmdb";
+
+    // Effectuer la recherche
+    async function handleSearch() {
+        if (query.trim()) {
+            goto(`/recherche?q=${encodeURIComponent(query)}`);
+        }
+    }
+
+
+
+</script>
+
 <div class="searchbar">
-    <input type="text" placeholder="Rechercher un film ..." name="" id="" />
+    <input type="text" placeholder="Rechercher un film ..." name="" id=""/>
         
     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
 
