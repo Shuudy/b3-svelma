6c020fe17998516e7bb47ebb2ff626414f373bd5
mnoella <mailholnoella@gmail.com>
2025-05-24 12:39:09 +0200

    feat: Add pagination for actor list in movie details

--- src/routes/films/[id]/+page.svelte
+++ src/routes/films/[id]/+page.svelte
@@ -1,9 +1,29 @@
 <script>
     import MovieDetails from "../../../components/MovieDetails.svelte";
     import ActorCard from "../../../components/ActorCard.svelte";
+	import Pagination from "../../../components/Pagination.svelte";
 
-    export let data;
+    const { data } = $props();
     const { movie, actors} = data;
+
+    // Pagination locale des acteurs
+	const ACTORS_PER_ROW = 5;
+	const ROWS_PER_PAGE = 2;
+	const ACTORS_PER_PAGE = ACTORS_PER_ROW * ROWS_PER_PAGE;
+	let currentActorsPage = $state(1);
+
+    // Calcul de la pagination
+	let totalActorsPages = $derived(Math.ceil(actors.length / ACTORS_PER_PAGE));
+	let paginatedActors = $derived(
+		actors.slice(
+			(currentActorsPage - 1) * ACTORS_PER_PAGE,
+			currentActorsPage * ACTORS_PER_PAGE
+		)
+	);
+
+    function handleActorsPageChange(page) {
+        currentActorsPage = page;
+    }
 
     console.log("Movie:", movie);
     console.log("Actors:", actors);
@@ -32,7 +52,7 @@
         <div class="actors">
             <div class="actors-title">Casting</div>
             <div class="actors-list">
-                {#each actors as actor, index}
+                {#each paginatedActors as actor}
                     <ActorCard
                         id={actor.id}
                         name={actor.name}
@@ -41,6 +61,13 @@
                     />
                 {/each}
             </div>
+            {#if totalActorsPages > 1}
+                <Pagination
+                    currentPage={currentActorsPage}
+                    totalPages={totalActorsPages}
+                    on:pageChange={e => handleActorsPageChange(e.detail)}
+                />
+            {/if}
         </div>
     </div>
 {:else}
